9b9ed8c5296f8ed36e152360278fd6fd597349bc
oonuma ryouyu <oonuma.ry0uyu@gmail.com>
2018-02-06 22:58:24 +0900

    cp 2 ln -s in install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
 echo "copy .zshrc & .gitconfig"
-cp ./.zshrc ~/
-cp ./.gitconfig ~/
-cp ./.vimrc ~/
+
+cp ~/.zshrc ~/.zshrc.bak
+rm ~/.zshrc
+ln -s ./.zshrc ~/
+
+cp ~/.gitconfig ~/.gitconfig.bak
+rm ~/.gitconfig
+ln -s ./.gitconfig ~/
+
+cp ~/.vimrc ~/.vimrc.bak
+rm ~/.vimrc
+ln -s ./.vimrc ~/
+
 echo "prepare dein vim"
 mkdir -p ~/.vim_runtime/repos/github.com/Shougo/dein.vim
 git clone https://github.com/Shougo/dein.vim.git \
