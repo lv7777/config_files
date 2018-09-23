2a4245c445242a9574c05607d8f660f3140d85bd
oonuma ryouyu <oonuma.ry0uyu@gmail.com>
2018-09-23 20:34:10 +0900

    [add] [linux/kali] [Y] add VSCODE Why: Detail:

--- bininstall.sh
+++ bininstall.sh
@@ -28,3 +28,23 @@ case $ANSER in
 	exit
 	break;;
 esac
+
+echo "do U install vscode? [Y/n]"
+read ANSER
+case $ANSER in
+	"" | "Y" | "y" | "yes" | "Yes" | "YES" )	
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install code # or code-insiders
+#OR
+#wget -o vscode.deb URL
+#dpdk -i vscode.deb
+#apt-get install -f
+	break;;
+	* )
+	exit
+	break;;
+esac
