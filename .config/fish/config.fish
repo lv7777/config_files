bd833c0e21693b930cb5dd03304745eeb03b9704
oonuma ryouyu <oonuma.ry0uyu@gmail.com>
2018-09-25 00:22:52 +0900

    [add] [linux] [y] porting fish alias from zsh Why: Detail:

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,24 @@
 set fish_theme fishface
+# alias TODO: porting alias from zsh
+alias viconf='nvim ~/.config/fish/config.fish'
+alias l='ls -ltr --color=auto'
+alias la='ls -la --color=auto'
+alias ll='ls -lah --color=auto'
+#alias so='source'
+alias sf='source ~/.config/fish/config.fish'
+alias v='nvim'
+alias g='git'
+alias gs='git status'
+#alias vi='vim'
+alias vf='nvim ~/.config/fish/config.fish'
+alias vv='vim ~/.vimrc'
+#alias c='cdr'
+# historyに日付を表示
+
+#alias cp='cp -i'
+alias rm='rm -i'
+alias mkdir='mkdir -p'
+alias diff='diff -U1'
+#if wsl...
+#alias cdwin="cd /mnt/c/Users/levena"
+alias du="du -h --max-depth=1"
