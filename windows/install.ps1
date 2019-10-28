84a212bc6502b74616ea3b086f4db687ae74dc1f
0num4 <oonuma.ryouyu@unipro.co.jp>
2019-10-29 02:46:52 +0900

    add install scripts for windows

--- windows/install.ps1
+++ windows/install.ps1
@@ -4,4 +4,7 @@
 
 #chromium is not enabled chrome account sync therefore use google chrome
 #powershell start-process "powershell 'choco install chromium'" -verb runas
-powershell start-process "powershell 'choco install googlechrome'" -verb runas
+powershell start-process "powershell 'choco install googlechrome -y'" -verb runas
+powershell start-process "powershell 'choco install cmder -y'" -verb runas
+powershell start-process "powershell 'choco install nodist -y'" -verb runas
+powershell start-process "powershell 'cinst visualstudiocode -y'" -verb runas
